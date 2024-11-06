2308b0b981964911c4380fe78f0308f59eabb34a
Danielle Ben shoshan <daniellebenshoshan@Danielles-MacBook-Pro.local>
2024-11-06 15:13:28 +0200

    commited

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,12 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+
+		int payment = Integer.parseInt(args[3]);
+		int FinalPayment = (int) Math.ceil(payment/3.0);
+		System.out.println("Dear "+ name3 + " , " + name2 + "and" + name1 + " : pay" + FinalPayment + " Shekels each.");
 	    // Replace this comment with the rest of your code   
 	}
 }
