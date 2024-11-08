647a55af211e81db9e4b19424d88294a883e4fa8
Danielle Ben shoshan <daniellebenshoshan@Danielles-MacBook-Pro.local>
2024-11-08 15:11:39 +0200

    HW1-done

--- Ascend.java
+++ Ascend.java
@@ -3,5 +3,17 @@
 public class Ascend {
 	public static void main(String[] args) {
 		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a = (int) (Math.random()*lim);
+		int b = (int) (Math.random()*lim);
+		int c = (int) (Math.random()*lim);
+		int minNum = Math.min((Math.min(a,b)), c); //useful, one line instead of more code lines
+		int maxNum = Math.max((Math.max(a, b)), c);
+		int middleNum = ((a+b+c) - (maxNum+minNum));
+
+		System.out.println(a+" "+b+" "+c);
+		System.out.println(minNum+" "+middleNum+" "+maxNum);
+		
+		
 	}
 }
